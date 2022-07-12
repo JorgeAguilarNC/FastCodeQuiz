a1cbf6feec42a05998121797f84137908b9a472e
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-07-12 13:03:02 -0400

    code base files2

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var startButton = document.querySelector(".startButton");
+//var to store quiz question
+
+startButton.addEventListener("click", function () {
+  alert("button clicked");
+});
+//mouse click event to start quiz
+//for loop to cycle through quiz questions
+//keypress event to recieve user input in the form of anseres to quiz questions
+//create a time limit for the game using functions
+//write conditional statements to determine wrong and right answers
+//client side storage to store high scores
